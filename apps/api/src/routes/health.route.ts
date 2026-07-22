3a180758b855b1ae0746db56d89e95c3ded49d5f
AmithMalinga <malingaamith1@gmail.com>
2026-07-22 12:10:28 +0530

    Initialize project architecture

---
+++ apps/api/src/routes/health.route.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (_, res) => {
+
+    res.json({
+        status: "OK",
+        service: "document-api"
+    });
+
+});
+
+
+export default router;
